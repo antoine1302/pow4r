fa06551008bf4f0c617cab7cd29131ab2bd72129
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-11-08 23:28:36 +0100

    Changes to be committed: 	modified:   grid.c

--- grid.c
+++ grid.c
@@ -6,23 +6,46 @@
 #define GRID_WIDTH 7
 #define GRID_HEIGTH 6
 
-static void display_grid_header();
+static void display_header();
+static void display_line();
 
 void display_grid()
 {
-    display_grid_header();
+    display_header();
+
+    for (unsigned i = 0; i < GRID_HEIGTH; i++) {
+
+        display_line();
+
+        fputs("|", stdout);
+
+        for (unsigned j = 0; j < GRID_WIDTH; j++) {
+            fputs("   |", stdout);    
+        }
+
+        fputs("\n", stdout);
+    }
+
+    display_header();
 }
 
-void display_grid_header()
+void display_header()
 {
     for (unsigned i = 0; i < GRID_WIDTH; i++ ) {
         printf("  %d ", i + 1);
     }
     
-    printf("\n+");
+    fputs("\n", stdout);
+}
+
+void display_line()
+{
+    fputs("+", stdout);
 
     for (unsigned i = 0; i < GRID_WIDTH; i++ ) {
-        printf("---+");
+        fputs("---+", stdout);
     }
+
+    fputs("\n", stdout);
 }
 

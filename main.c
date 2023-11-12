ad040b119f26f2fff1a02c3206c6103baa7f5f21
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-11-12 04:40:35 +0100

    Add retry logic

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 struct player player_collection[] = {{1, PLAYER_1_TOKEN}, {2, PLAYER_2_TOKEN}};
 unsigned player_amount = sizeof player_collection / sizeof (struct player);
+static _Bool is_column_valid(unsigned, unsigned (*grid)[GRID_WIDTH]);
 
 int main()
 {
@@ -13,19 +14,28 @@ int main()
     while (1) {
         for (size_t i = 0; i < player_amount; i++) {
 
-            system("clear");
-
             display_grid(grid);
             printf("Player %u: ", player_collection[i].id);
+            system("clear");
+            _Bool is_input_valid = 0;
             unsigned column = 0;
-            int ret = scanf("%u", &column);
 
-            // control overflow column 
+            do {
+                unsigned input = scanf("%u", &column);
 
-            if(ret < 1) {
-                puts("Failed to get column");
-                return EXIT_FAILURE;
-            }
+                if(input < 1) {
+                    puts("Failed to get column. Please retry.");
+                    // clear buffer
+                    continue;
+                }
+
+                if(!is_column_valid(input, grid)) {
+                    continue;
+                }
+
+                is_input_valid = 1;
+
+            } while(!is_input_valid);
 
             for(int j = GRID_HEIGTH - 1; j >= 0; j--) {
                 if (grid[j][column - 1] < 1) {
@@ -39,5 +49,21 @@ int main()
     }
 
     return EXIT_SUCCESS;
+}
+
+_Bool is_column_valid(unsigned column, unsigned (*grid)[GRID_WIDTH])
+{
+    // check for out of range column
+    if(column < 1 || column > GRID_WIDTH) {
+        puts("Out of range value");
+        return 0;
+    }
+    // check if the column is already full
+    if(grid[GRID_HEIGTH -1][GRID_WIDTH -1] > 0) {
+        puts("Column already full");
+        return 0;
+    }
+
+    return 1;
 }
  

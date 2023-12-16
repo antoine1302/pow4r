7cee401a71075e3102d6662713f6241dd456a1ad
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-12-16 16:53:14 +0100

    Refacto robot logic
     Changes to be committed:
    	new file:   main
    	new file:   random
    	modified:   random.c
    	modified:   robot
    	modified:   robot.c

--- robot.c
+++ robot.c
@@ -11,7 +11,7 @@
 static unsigned findMaxAlignmentByPosition(struct position *, unsigned, unsigned (*)[GRID_WIDTH]);
 static unsigned findMaxAlignmentByDirection(struct position, int , int, unsigned, unsigned (*)[GRID_WIDTH]);
 static _Bool isPositionValid(struct position *);
-static unsigned get_max(unsigned, ...);
+static unsigned get_max(int, ...);
 
 void get_robot_input(unsigned *column, unsigned (*grid)[GRID_WIDTH])
 {
@@ -37,11 +37,11 @@ void get_robot_input(unsigned *column, unsigned (*grid)[GRID_WIDTH])
         max_alignment = findMaxAlignmentByPosition(&current_pos, PLAYER_2_ID, grid);
 
         if (max_alignment >= WIN_ALIGNMENT) {
-            *column = i;
+            *column = i+1;
             return;
         }
 
-        max_alignment = get_max(max_alignment, findMaxAlignmentByPosition(&current_pos, PLAYER_1_ID, grid));
+        max_alignment = get_max(2, max_alignment, findMaxAlignmentByPosition(&current_pos, PLAYER_1_ID, grid));
 
         if (max_alignment >= global_max_alignment)
         {
@@ -55,16 +55,17 @@ void get_robot_input(unsigned *column, unsigned (*grid)[GRID_WIDTH])
         }
     }
 
-    *column = best_column[random_int(0, count_per_alignment - 1)];
+    *column = best_column[random_int(0, (int)count_per_alignment - 1)] + 1;
 }
 
 unsigned findMaxAlignmentByPosition(struct position *pos, unsigned player_id, unsigned (*grid)[GRID_WIDTH])
 {
     return  get_max(
-        findMaxAlignmentByDirection(*pos, 1, 0, player_id, grid),
-        findMaxAlignmentByDirection(*pos, 0, 1, player_id, grid) + findMaxAlignmentByDirection(*pos, 0, -1, player_id, grid) - 1,
-        findMaxAlignmentByDirection(*pos, 1, 1, player_id, grid) + findMaxAlignmentByDirection(*pos, -1, -1, player_id, grid) - 1,
-        findMaxAlignmentByDirection(*pos, 1, -1, player_id, grid) + findMaxAlignmentByDirection(*pos, -1, 1, player_id, grid) - 1
+            4,
+            findMaxAlignmentByDirection(*pos, 1, 0, player_id, grid),
+            findMaxAlignmentByDirection(*pos, 0, 1, player_id, grid) + findMaxAlignmentByDirection(*pos, 0, -1, player_id, grid) - 1,
+            findMaxAlignmentByDirection(*pos, 1, 1, player_id, grid) + findMaxAlignmentByDirection(*pos, -1, -1, player_id, grid) - 1,
+            findMaxAlignmentByDirection(*pos, 1, -1, player_id, grid) + findMaxAlignmentByDirection(*pos, -1, 1, player_id, grid) - 1
         );
 }
 
@@ -99,20 +100,20 @@ _Bool isPositionValid(struct position *pos)
     return 1;
 }
 
-unsigned get_max(unsigned arg_count, ...)
+unsigned get_max(int num, ...)
 {
-    va_list ap;
-    va_start(ap, arg_count);
-    unsigned current_val = 0;
-    unsigned max = va_arg(ap, unsigned);
+    va_list args;
+    va_start(args, num);
+    unsigned max = va_arg(args, unsigned);
 
-    for (unsigned i = 2; i <= arg_count; i++) {
-        if ((current_val = va_arg(ap, unsigned)) > max) {
+    for (int i = 1; i < num; i++) {
+        unsigned current_val = va_arg(args, unsigned);
+        if (current_val > max) {
             max = current_val;
         }
     }
 
-    va_end(ap);
+    va_end(args);
 
     return max;
 }
